b3ef77fa40b5f75bb4424b83d454b635d3827af8
Thomas Manson <dev.mansonthomas@gmail.com>
2026-04-13 15:44:12 +0200

    fix: correct rcqV1Url domain in environment configurations
    Agent-Id: agent-35304b37-eeef-4130-9432-017a2569ab72
    Linked-Note-Id: 32e4c2d7-54ed-445f-89ca-f81a5a21e6be

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://dev.back.graph.redcrossquest.com',
   supersetUrl: 'https://dev.superset.graph.redcrossquest.com',
-  rcqV1Url: 'https://devredcrossquest.croix-rouge.fr',
+  rcqV1Url: 'https://dev.redcrossquest.croix-rouge.fr',
   // Set to true to enable Superset embedded dashboards (requires Superset Cloud Run service)
   enableSuperset: false,
 };
