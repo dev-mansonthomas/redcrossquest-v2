624c452ed1df0d209a004d6d86e4602b1678b09a
Thomas Manson <dev.mansonthomas@gmail.com>
2026-05-15 02:24:26 +0200

    fix(env): propagate YEARLY_GOALS_DYNAMIC_START_DATE through generate-env.sh (#19)
    Agent-Id: agent-b1d2a86a-e3f4-4c29-8251-a07c702b08fb

--- scripts/generate-env.sh
+++ scripts/generate-env.sh
@@ -84,6 +84,7 @@ $(write_var SUPERSET_DASHBOARD_YEARLY_GOAL)
 $(write_var CORS_ORIGINS)
 FRONTEND_URL=${BACKEND_FRONTEND_URL}
 $(write_var DEBUG)
+$(write_var YEARLY_GOALS_DYNAMIC_START_DATE)
 $(write_var VALKEY_HOST)
 $(write_var VALKEY_PORT)
 $(write_var VALKEY_DB)
